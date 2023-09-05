8f61721c00020e8c71e544bd6a83c8691fd20c48
VitaliiChichuga <chichygav@gmail.com>
2023-09-05 23:57:57 +0300

    add

---
+++ js/modal.js
@@ -0,0 +1,12 @@
+const modalContent = document.querySelector('.modal-content');
+
+const btn = document.getElementById('myBtn');
+
+btn.onclick = function () {
+  modalContent.classList.toggle('show-modal-content');
+};
+document.onclick = function (e) {
+  if (e.target.closest('.qwe')) {
+    modalContent.classList.remove('show-modal-content');
+  }
+};
